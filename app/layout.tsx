7ed054d0118f9262a10525d72bc32dfc4d85c5fc
hajimss <hazim897@gmail.com>
2026-04-04 00:15:43 +0800

    Revert "feat: switch script font from Pinyon Script to Great Vibes"
    This reverts commit b2eeb85105b007c6bfe7d0ccd9f9f32cea4a278b.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from 'next'
-import { Great_Vibes, Cormorant_Garamond, Josefin_Sans } from 'next/font/google'
+import { Pinyon_Script, Cormorant_Garamond, Josefin_Sans } from 'next/font/google'
 import { LanguageProvider } from '@/lib/language-context'
 import './globals.css'
 
-const greatVibes = Great_Vibes({
+const pinyonScript = Pinyon_Script({
   weight: '400',
   subsets: ['latin'],
-  variable: '--font-great-vibes',
+  variable: '--font-pinyon',
 })
 
 const cormorant = Cormorant_Garamond({
@@ -31,7 +31,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body
-        className={`${greatVibes.variable} ${cormorant.variable} ${josefinSans.variable} font-sans`}
+        className={`${pinyonScript.variable} ${cormorant.variable} ${josefinSans.variable} font-sans`}
       >
         <LanguageProvider>{children}</LanguageProvider>
       </body>
